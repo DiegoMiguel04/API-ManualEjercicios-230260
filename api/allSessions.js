bb6162ade358e15b89c2edcf48cdd49a3673dd41
Diego Miguel Rivera Chávez <230260@utxicotepec.edu.mx>
2025-03-15 16:23:08 -0600

    API
    Implementacion de CORS en los endpoints

--- api/allSessions.js
+++ api/allSessions.js
@@ -1,8 +1,24 @@
 import { MongoClient } from "mongodb";
+import Cors from "cors";
+
+const cors = Cors({
+  methods: ['GET', 'POST'],
+});
 
 const client = new MongoClient('mongodb+srv://xkis-db:1234@api-awi-40-230260.yp44i.mongodb.net/?retryWrites=true&w=majority&appName=API-AWI-40-230260');
 
+const runCors = (req, res) => new Promise((resolve, reject) => {
+  cors(req, res, (result) => {
+    if (result instanceof Error) {
+      reject(result);
+    }
+    resolve(result);
+  });
+});
+
 export default async (req, res) => {
+  await runCors(req, res);
+
   if (req.method === 'GET') {
     try {
       await client.connect();
